993c36d67f8e3daaf18dd618d922d28f3d1d6247
Alex Kapelyukhovsky <alkapa2005@gmail.com>
2020-11-26 11:34:39 +0200

    Add GameControllerActor

---
+++ Game.ActorModel/Actors/GameControllerActor.cs
@@ -0,0 +1,41 @@
+﻿using Akka.Actor;
+using Game.ActorModel.Messages;
+using System;
+using System.Collections.Generic;
+
+namespace Game.ActorModel.Actors
+{
+    public class GameControllerActor : ReceiveActor
+    {
+        private readonly Dictionary<string, IActorRef> _players;
+        public GameControllerActor()
+        {
+            _players = new Dictionary<string, IActorRef>();
+
+            Receive<JoinGameMessage>(JoinGame);
+            Receive<AttackPlayerMessage>(HandleAttackPlayerMessage);
+        }
+
+        private void HandleAttackPlayerMessage(AttackPlayerMessage message)
+        {
+            _players[message.PlayerName].Forward(message);
+        }
+
+        private void JoinGame(JoinGameMessage message)
+        {
+            bool playerNeedsCreating = !_players.ContainsKey(message.PlayerName);
+
+            if(playerNeedsCreating)
+            {
+                IActorRef newPlayerActor = Context.ActorOf(
+                    Props.Create(() => new PlayerActor(message.PlayerName)), message.PlayerName);
+
+                _players.Add(message.PlayerName, newPlayerActor);
+                foreach (var player in _players.Values)
+                {
+                    player.Tell(new RefreshPlayerStatusMessage(), Sender);
+                }
+            }
+        }
+    }
+}
