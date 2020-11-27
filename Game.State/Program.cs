8ed295339f4e54f2d8397d1ea6e244abf9bf38e7
Alex Kapelyukhovsky <alkapa2005@gmail.com>
2020-11-28 00:00:34 +0200

    Add Game.State console app

---
+++ Game.State/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Akka.Actor;
+using Game.ActorModel.Actors;
+
+namespace Game.State
+{
+    class Program
+    {
+
+        private static ActorSystem ActorSystemInstance;
+
+        static void Main(string[] args)
+        {
+            ActorSystemInstance = ActorSystem.Create("GameSystem");
+
+            var gameController = ActorSystemInstance.ActorOf<GameControllerActor>("GameController");
+
+            Console.ReadKey();
+        }
+    }
+}
